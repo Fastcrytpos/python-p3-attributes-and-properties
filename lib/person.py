e1dd04ae938d1f02ee30abd0c6a5dab187383463
Samson Mubeya <rasspartah@gmail.com>
2024-05-31 23:40:32 +0300

    learnt the decorator and property function in py

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    def __init__(self,name="James",job="ITC"):
+        self.name=name
+        self.job=job
+
+    @property
+    def name(self):
+        return self._name
+
+
+    @property
+    def job(self):
+        return self._job
+
+    @name.setter
+    def name(self,name):
+        if isinstance(name, str) and 0 < len(name) < 25:
+           self._name=name.title()
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    @job.setter
+    def job(self,job):
+        if job in APPROVED_JOBS:
+            self._job=job
+        
+        else:
+            print("Job must be in list of approved jobs.")
+            
     pass
